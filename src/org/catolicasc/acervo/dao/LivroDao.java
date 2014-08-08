2ce41546a64f245132ffa313666a8159da4e30f0
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-08-08 20:50:31 -0300

    Acervo

--- src/org/catolicasc/acervo/dao/LivroDao.java
+++ src/org/catolicasc/acervo/dao/LivroDao.java
@@ -1,13 +1,33 @@
 package org.catolicasc.acervo.dao;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.catolicasc.acervo.model.Livro;
 
 public class LivroDao {
 
-	public List<Livro> obterLivro() {
-		return null;
+	public List<Livro> obterLivros() {
+		List<Livro> livros = new ArrayList<Livro>();
+		
+		Livro livro1 = new Livro();
+		livro1.setEditora("Laranjeiras");
+		livro1.setAno(2015);
+		livro1.setNome("Fluminense - A conquista");
+		livro1.setResumo("A história da conquista da libertadores da américa");
+		livro1.setAutores(Arrays.asList("Fred", "Conca", "Cícero"));
+		livros.add(livro1);
+		
+		Livro livro2 = new Livro();
+		livro2.setEditora("Maracanã");
+		livro2.setAno(2014);
+		livro2.setNome("Fluminense - Brasileirão");
+		livro2.setResumo("Fluminense: Penta campeão brasileiro");
+		livro2.setAutores(Arrays.asList("Cavalieri", "Jean", "Wágner"));		
+		livros.add(livro2);
+		
+		return livros;
 	}
 
 }
--- src/org/catolicasc/acervo/service/ListagemLivros.java
+++ src/org/catolicasc/acervo/service/ListagemLivros.java
@@ -2,14 +2,23 @@ package org.catolicasc.acervo.service;
 
 import java.util.List;
 
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
 import org.catolicasc.acervo.dao.LivroDao;
 import org.catolicasc.acervo.model.Livro;
 
+@WebService
 public class ListagemLivros {
 	
 	public List<Livro> listarLIvros(){
 		LivroDao dao = new LivroDao();
-		return dao.obterLivro();		
+		return dao.obterLivros();		
+	}
+	
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:8180/livros", new ListagemLivros());
+		System.out.println("Serviço inicializado!");
 	}
 
 }
