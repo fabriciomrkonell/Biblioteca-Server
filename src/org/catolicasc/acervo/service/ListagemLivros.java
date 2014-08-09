5574ac2f64e8f613a10ffddd714e750910bddac6
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-08-08 21:15:35 -0300

    Acervo

--- src/org/catolicasc/acervo/service/ListagemLivros.java
+++ src/org/catolicasc/acervo/service/ListagemLivros.java
@@ -17,7 +17,7 @@ public class ListagemLivros {
 	}
 	
 	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:8180/livros", new ListagemLivros());
+		Endpoint.publish("http://localhost:8180/livros", new ListagemLivros());		
 		System.out.println("Serviço inicializado!");
 	}
 
